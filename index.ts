e75496f9f24c9bb6d782f495397c188126add157
Konrad Guzek <konrad@guzek.uk>
2024-12-20 22:58:27 +0100

    simplify route setup

--- index.ts
+++ index.ts
@@ -6,12 +6,12 @@ import { getLogger } from "guzek-uk-common/logger";
 import { getMiddleware } from "guzek-uk-common/middleware";
 import { startServer } from "guzek-uk-common/server";
 import { send405 } from "guzek-uk-common/util";
+import { router as authRouter } from "./src/routes/auth";
+import { router as wellKnownRouter } from "./src/routes/.well-known";
 
 const logger = getLogger(__filename);
 
 const app = express();
-
-const ENDPOINTS = ["auth", ".well-known"];
 
 async function initialise() {
   const iterations = process.env.HASH_ITERATIONS;
@@ -24,11 +24,8 @@ async function initialise() {
   password.pepper(process.env.HASH_PEPPER);
 
   app.use(getMiddleware());
-  for (const endpoint of ENDPOINTS) {
-    const middleware = await import("./src/routes/" + endpoint);
-    if (middleware.init) middleware.init(ENDPOINTS);
-    app.use(`/${endpoint}`, middleware.router, send405);
-  }
+  app.use("/auth", authRouter, send405);
+  app.use("/.well-known", wellKnownRouter, send405);
 
   startServer(app);
 }
